50ce2d9ac79047bdf12cb252cc1a90b5802040f3
KookaS <olivier.charrez@hotmail.com>
2022-06-26 23:29:49 +0200

    add other image collections to selection

--- src/utils/collection.go
+++ src/utils/collection.go
@@ -11,6 +11,10 @@ func ImageCollectionSelection (mongoClient *mongo.Client, selection string) (*mo
 	switch selection {
 	case "flickr":
 		return mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("FLICKR_COLLECTION")), nil
+	case "unsplash":
+		return mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("UNSPLASH_COLLECTION")), nil
+	case "pexels":
+		return mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("PEXELS_COLLECTION")), nil
 	default:
 		return nil, errors.New("Collection does not exist")
 	}
@@ -20,5 +24,7 @@ func ImageCollectionSelection (mongoClient *mongo.Client, selection string) (*mo
 func ImageCollections (mongoClient *mongo.Client) map[string]*mongo.Collection {
 	collections := make(map[string]*mongo.Collection)
 	collections["flickr"] = mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("FLICKR_COLLECTION"))
+	collections["unsplash"] = mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("UNSPLASH_COLLECTION"))
+	collections["pexels"] = mongoClient.Database(DotEnvVariable("SCRAPPER_DB")).Collection(DotEnvVariable("PEXELS_COLLECTION"))
 	return collections
 }
