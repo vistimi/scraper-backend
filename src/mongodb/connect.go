4b1bc2a50580cbf5ea203452ccd6c1cb363ba555
KookaS <olivier.charrez@hotmail.com>
2022-07-11 16:16:32 +0200

    sdk

--- src/mongodb/connect.go
+++ src/mongodb/connect.go
@@ -1,6 +1,7 @@
 package mongodb
 
 import (
+	"log"
 	"scraper/src/utils"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -9,18 +10,21 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo/options"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func Connect() *mongo.Client {
-
+func ConnectMongoDB() *mongo.Client {
 	uri := utils.DotEnvVariable("MONGODB_URI")
 
 	// context, cancel := context.WithTimeout(context.TODO(), 20*time.Second)
 	// defer cancel()
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// defer func() {
@@ -31,9 +35,36 @@ func Connect() *mongo.Client {
 
 	// Ping the primary
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	return client
+}
+
+func ConnectDynamoDB() *dynamodb.Client {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
-	return client
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
 
+	// Build the request with its input parameters
+	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	log.Println("Tables:")
+	for _, tableName := range resp.TableNames {
+		log.Println(tableName)
+	}
+	return svc
 }
