22c49c69921a83ffb9cacd30e68275ec732ad09e
KookaS <olivier.charrez@hotmail.com>
2022-07-22 17:10:55 +0200

    test

--- src/main.go
+++ src/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"scraper/src/mongodb"
-	"scraper/src/router"
-	"scraper/src/utils"
+	// "scraper/src/mongodb"
+	// "scraper/src/router"
+	// "scraper/src/utils"
 )
 
 func main() {
-
-	fmt.Println(utils.DotEnvVariable("SCRAPER_DB"))
-	fmt.Println(utils.DotEnvVariable("IMAGE_PATH"))
-	fmt.Println(utils.DotEnvVariable("TAGS_UNWANTED_COLLECTION"))
-	fmt.Println(utils.DotEnvVariable("TAGS_WANTED_COLLECTION"))
-	mongoClient := mongodb.ConnectMongoDB()
-	_ = router.Router(mongoClient)
+	fmt.Print("Hello AWS")
+	// fmt.Println(utils.DotEnvVariable("SCRAPER_DB"))
+	// fmt.Println(utils.DotEnvVariable("IMAGE_PATH"))
+	// fmt.Println(utils.DotEnvVariable("TAGS_UNWANTED_COLLECTION"))
+	// fmt.Println(utils.DotEnvVariable("TAGS_WANTED_COLLECTION"))
+	// mongoClient := mongodb.ConnectMongoDB()
+	// _ = router.Router(mongoClient)
 }
