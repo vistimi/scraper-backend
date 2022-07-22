a9871b203ea6970b0b7e4715742eac88b1fd2dce
KookaS <olivier.charrez@hotmail.com>
2022-07-22 18:12:00 +0200

    env not in file

--- src/utils/env.go
+++ src/utils/env.go
@@ -3,15 +3,10 @@ package utils
 import (
 	"log"
 	"os"
-	"github.com/joho/godotenv"
 )
 
 // DotEnvVariable use godot package to load/read the .env file and return the value of the key
 func DotEnvVariable(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 	varEnv := os.Getenv(key)
 	if varEnv == "" {
 		log.Fatalf("Empty env variable %s", key)
